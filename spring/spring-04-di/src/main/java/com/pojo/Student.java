2d175972064b9fa798f30c35355b55259a63aa6c
TanaStudy <tanzhihan3@gmail.com>
2021-09-22 21:49:24 +0800

    di

---
+++ spring/spring-04-di/src/main/java/com/pojo/Student.java
@@ -0,0 +1,96 @@
+package com.pojo;
+
+import java.util.*;
+
+public class Student {
+
+    private String name;
+    private Address address;
+
+    private String[] books;
+    private List<String> hobbies;
+
+    private Map<String, String> card;
+    private Set<String> game;
+
+    private Properties infor;
+    private String wife;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", address=" + address +
+                ", books=" + Arrays.toString(books) +
+                ", hobbies=" + hobbies +
+                ", card=" + card +
+                ", game=" + game +
+                ", infor=" + infor +
+                ", wife='" + wife + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String[] getBooks() {
+        return books;
+    }
+
+    public void setBooks(String[] books) {
+        this.books = books;
+    }
+
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(List<String> hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public Map<String, String> getCard() {
+        return card;
+    }
+
+    public void setCard(Map<String, String> card) {
+        this.card = card;
+    }
+
+    public Set<String> getGame() {
+        return game;
+    }
+
+    public void setGame(Set<String> game) {
+        this.game = game;
+    }
+
+    public Properties getInfor() {
+        return infor;
+    }
+
+    public void setInfor(Properties infor) {
+        this.infor = infor;
+    }
+
+    public String getWife() {
+        return wife;
+    }
+
+    public void setWife(String wife) {
+        this.wife = wife;
+    }
+}
