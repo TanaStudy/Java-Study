3362311db556bbbe69b62f71938492ab26e3d8e0
TanaStudy <tanzhihan3@gmail.com>
2021-10-07 10:25:02 +0800

    mybatis

---
+++ spring/spring-10-mybatis/src/main/java/com/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.mapper;
+
+import com.pojo.User;
+
+import java.util.List;
+
+public interface UserMapper {
+    List<User> selectUser();
+
+
+}
---
+++ spring/spring-10-mybatis/src/main/java/com/pojo/User.java
@@ -0,0 +1,16 @@
+package com.pojo;
+
+public class User {
+    private int id;
+    private String name;
+    private String pwd;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", pwd='" + pwd + '\'' +
+                '}';
+    }
+}
