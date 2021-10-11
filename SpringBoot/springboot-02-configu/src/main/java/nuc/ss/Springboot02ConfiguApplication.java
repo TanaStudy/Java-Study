00177d4a9ed68c4814954f82569a608337b48d65
TanaStudy <tanzhihan3@gmail.com>
2021-10-11 10:18:25 +0800

    configu

---
+++ SpringBoot/springboot-02-configu/src/main/java/nuc/ss/Springboot02ConfiguApplication.java
@@ -0,0 +1,13 @@
+package nuc.ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot02ConfiguApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot02ConfiguApplication.class, args);
+    }
+
+}
