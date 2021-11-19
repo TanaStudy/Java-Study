d1866c2d746fb5e7fb37b3bc46ac1fde53182905
TanaStudy <tanzhihan3@gmail.com>
2021-11-19 17:41:00 +0800

    Q

--- LeetCode/src/leetcode/editor/cn/Q206反转链表.java
+++ LeetCode/src/leetcode/editor/cn/Q206反转链表.java
@@ -75,19 +75,21 @@ class Solution {
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
-
-// 方法一、迭代
-public ListNode reverseList(ListNode head) {
-	ListNode pre = null;
-	ListNode cur = head;
-	while(cur != null){
-		ListNode temp = cur.next;
-		cur.next = pre;
-		pre = cur;
-		cur = temp;
+class Solution1{
+	// 方法一、迭代
+	public ListNode reverseList(ListNode head) {
+		ListNode pre = null;
+		ListNode cur = head;
+		while(cur != null){
+			ListNode temp = cur.next;
+			cur.next = pre;
+			pre = cur;
+			cur = temp;
+		}
+		return pre;
 	}
-	return pre;
 }
+
 }
 
 
