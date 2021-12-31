90962a590263d40d15210c6fc912ad668c2f85e9
TanaStudy <tanzhihan3@gmail.com>
2021-12-31 09:10:50 +0800

    Q39

---
+++ 9. LeetCode/src/leetcode/editor/cn/Q39组合总和.java
@@ -0,0 +1,93 @@
+package leetcode.editor.cn;
+
+//给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，找出 candidates 中可以使数字和为目标数 target 的
+// 所有不同组合 ，并以列表形式返回。你可以按 任意顺序 返回这些组合。 
+//
+// candidates 中的 同一个 数字可以 无限制重复被选取 。如果至少一个数字的被选数量不同，则两种组合是不同的。 
+//
+// 对于给定的输入，保证和为 target 的不同组合数少于 150 个。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：candidates = [2,3,6,7], target = 7
+//输出：[[2,2,3],[7]]
+//解释：
+//2 和 3 可以形成一组候选，2 + 2 + 3 = 7 。注意 2 可以使用多次。
+//7 也是一个候选， 7 = 7 。
+//仅有这两种组合。 
+//
+// 示例 2： 
+//
+// 
+//输入: candidates = [2,3,5], target = 8
+//输出: [[2,2,2,2],[2,3,3],[3,5]] 
+//
+// 示例 3： 
+//
+// 
+//输入: candidates = [2], target = 1
+//输出: []
+// 
+//
+// 示例 4： 
+//
+// 
+//输入: candidates = [1], target = 1
+//输出: [[1]]
+// 
+//
+// 示例 5： 
+//
+// 
+//输入: candidates = [1], target = 2
+//输出: [[1,1]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= candidates.length <= 30 
+// 1 <= candidates[i] <= 200 
+// candidate 中的每个元素都 互不相同 
+// 1 <= target <= 500 
+// 
+// Related Topics 数组 回溯 👍 1675 👎 0
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q39组合总和{
+	public static void main(String[] args) {
+		Solution solution = new Q39组合总和().new Solution();
+		
+	}
+//leetcode submit region begin(Prohibit modification and deletion)
+// https://leetcode-cn.com/problems/combination-sum/solution/hui-su-suan-fa-jian-zhi-python-dai-ma-java-dai-m-2/
+class Solution {
+	List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+		back(candidates, target, new ArrayList<>(), 0);
+		return res;
+    }
+    public void back(int[] candidates, int target, List<Integer> path, int index){
+    	if(target < 0){
+    		return;
+		}
+    	if(target == 0){
+    		res.add(new ArrayList<>(path));
+		}
+    	for(int i = index; i < candidates.length; i++){
+    		path.add(candidates[i]);
+    		back(candidates, target - candidates[i], path , i);
+    		path.remove(path.size() - 1);
+		}
+	}
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
