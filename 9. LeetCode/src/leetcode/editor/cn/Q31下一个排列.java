ee59c17cadc7054712c502cec628577e6e26b3d5
TanaStudy <tanzhihan3@gmail.com>
2022-01-04 10:10:34 +0800

    Q31

---
+++ 9. LeetCode/src/leetcode/editor/cn/Q31下一个排列.java
@@ -0,0 +1,62 @@
+package leetcode.editor.cn;
+
+//实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列（即，组合出下一个更大的整数）。 
+//
+// 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。 
+//
+// 必须 原地 修改，只允许使用额外常数空间。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,2,3]
+//输出：[1,3,2]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,2,1]
+//输出：[1,2,3]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [1,1,5]
+//输出：[1,5,1]
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：nums = [1]
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 100 
+// 0 <= nums[i] <= 100 
+// 
+// Related Topics 数组 双指针 👍 1463 👎 0
+
+public class Q31下一个排列{
+	public static void main(String[] args) {
+		Solution solution = new Q31下一个排列().new Solution();
+		
+	}
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void nextPermutation(int[] nums) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
