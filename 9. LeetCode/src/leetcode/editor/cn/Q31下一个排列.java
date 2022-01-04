c0cbf36fd32559686d3d17463b3b0bf131104b88
TanaStudy <tanzhihan3@gmail.com>
2022-01-04 12:00:43 +0800

    Q31

--- 9. LeetCode/src/leetcode/editor/cn/Q31下一个排列.java
+++ 9. LeetCode/src/leetcode/editor/cn/Q31下一个排列.java
@@ -49,13 +49,42 @@ package leetcode.editor.cn;
 public class Q31下一个排列{
 	public static void main(String[] args) {
 		Solution solution = new Q31下一个排列().new Solution();
+		int[] nums = new int[]{1,1};
+		solution.nextPermutation(nums);
 		
 	}
 //leetcode submit region begin(Prohibit modification and deletion)
+// 方法：https://leetcode-cn.com/problems/next-permutation/solution/xia-yi-ge-pai-lie-suan-fa-xiang-jie-si-lu-tui-dao-/
+// 代码：https://leetcode-cn.com/problems/next-permutation/solution/xia-yi-ge-pai-lie-by-leetcode-solution/
 class Solution {
     public void nextPermutation(int[] nums) {
+		int i = nums.length - 2;
+		while (i >= 0 && nums[i] >= nums[i+1]){
+			i--;
+		}
+		if(i >= 0){
+			int j = nums.length - 1;
+			while (j >= 0 && nums[i] >= nums[j]){
+				j--;
+			}
+			swap(nums,i,j);
+		}
+		reverse(nums,i+1);
 
     }
+    public void swap(int[] nums, int i, int j){
+    	int temp = nums[i];
+    	nums[i] = nums[j];
+    	nums[j] = temp;
+	}
+	public void reverse(int[] nums, int start){
+    	int end = nums.length - 1;
+    	while (start < end){
+    		swap(nums, start, end);
+    		start++;
+    		end--;
+		}
+	}
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
