612ccced4425359181e37ce8636bd14ffd82bc52
TanaStudy <tanzhihan3@gmail.com>
2022-01-06 16:21:22 +0800

    冒泡排序

---
+++ 8. Demo/Java/src/sort/冒泡排序.java
@@ -0,0 +1,26 @@
+package sort;
+
+import java.util.Arrays;
+
+// https://blog.csdn.net/weixin_41190227/article/details/86600821
+// 两两交换，每一轮都把子数组中最大的元素交换到子数组末尾
+public class 冒泡排序 {
+    public static int[] bubbleSort(int[] nums){
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < nums.length - 1 - i; j++){
+                if(nums[j] > nums[j+1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+                System.out.println(Arrays.toString(nums));
+            }
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{7,10,2,4,7,1,8,5,19};
+        System.out.println(Arrays.toString(bubbleSort(nums)));
+    }
+}
