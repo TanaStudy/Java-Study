c373cb4383ed10f00d0cfd46a0d0ea9395449b5c
TanaStudy <tanzhihan3@gmail.com>
2022-01-07 12:18:05 +0800

    选择排序

---
+++ 8. Demo/Java/src/sort/选择排序.java
@@ -0,0 +1,28 @@
+package sort;
+
+import java.util.Arrays;
+
+// https://blog.csdn.net/weixin_41190227/article/details/86600821
+// https://www.runoob.com/w3cnote/selection-sort.html
+// 每一轮找到最小的数，放在数组的前端
+public class 选择排序 {
+    public static int[] selectionSort(int[] nums){
+        for(int i = 0; i < nums.length - 1; i++){
+            int minIndex = i;
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[j] < nums[minIndex]){
+                    minIndex = j;
+                }
+            }
+            int temp = nums[i];
+            nums[i] = nums[minIndex];
+            nums[minIndex] = temp;
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{7,10,2,4,7,1,8,5,19};
+        System.out.println(Arrays.toString(selectionSort(nums)));
+    }
+}
