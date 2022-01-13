d91da69323af5233dd10f0ae0333e2a31aa69013
TanaStudy <tanzhihan3@gmail.com>
2022-01-13 11:50:29 +0800

    插入排序

---
+++ 8. Demo/Java/src/sort/插入排序.java
@@ -0,0 +1,25 @@
+package sort;
+
+import java.util.Arrays;
+
+// https://blog.csdn.net/weixin_41190227/article/details/86600821
+// 每一轮将当前元素插入到已排序的数组中
+public class 插入排序 {
+    public static int[] insertionSort(int[] nums){
+        for(int i = 0; i < nums.length - 1; i++){
+            int cur = nums[i + 1];
+            int preIndex = i;
+            while (preIndex >= 0 && cur < nums[preIndex]){
+                nums[preIndex + 1] = nums[preIndex];
+                preIndex--;
+            }
+            nums[preIndex + 1] = cur;
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{7,10,2,4,7,1,8,5,19};
+        System.out.println(Arrays.toString(insertionSort(nums)));
+    }
+}
