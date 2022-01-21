8723b3902ee374d0747c3ff64717068650294c60
TanaStudy <tanzhihan3@gmail.com>
2022-01-22 00:01:02 +0800

    Q104

---
+++ 9. LeetCode/src/leetcode/editor/cn/Q104二叉树的最大深度.java
@@ -0,0 +1,53 @@
+package leetcode.editor.cn;
+
+//给定一个二叉树，找出其最大深度。 
+//
+// 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。 
+//
+// 说明: 叶子节点是指没有子节点的节点。 
+//
+// 示例： 
+//给定二叉树 [3,9,20,null,null,15,7]， 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7 
+//
+// 返回它的最大深度 3 。 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1061 👎 0
+
+public class Q104二叉树的最大深度{
+	public static void main(String[] args) {
+		Solution solution = new Q104二叉树的最大深度().new Solution();
+		
+	}
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+    	if(root == null){
+    		return 0;
+		}
+    	return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
