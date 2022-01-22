b02a574dbb47afbc9d9e71f252dffdb1e8dff79a
TanaStudy <tanzhihan3@gmail.com>
2022-01-22 11:46:34 +0800

    Q122

---
+++ 9. LeetCode/src/leetcode/editor/cn/Q122买卖股票的最佳时机 II.java
@@ -0,0 +1,75 @@
+package leetcode.editor.cn;
+
+//给定一个数组 prices ，其中 prices[i] 是一支给定股票第 i 天的价格。 
+//
+// 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。 
+//
+// 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: prices = [7,1,5,3,6,4]
+//输出: 7
+//解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+//     随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3 。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: prices = [1,2,3,4,5]
+//输出: 4
+//解释: 在第 1 天（股票价格 = 1）的时候买入，在第 5 天 （股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+//     注意你不能在第 1 天和第 2 天接连购买股票，之后再将它们卖出。因为这样属于同时参与了多笔交易，你必须在再次购买前出售掉之前的股票。
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: prices = [7,6,4,3,1]
+//输出: 0
+//解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= prices.length <= 3 * 10⁴ 
+// 0 <= prices[i] <= 10⁴ 
+// 
+// Related Topics 贪心 数组 动态规划 👍 1486 👎 0
+
+import java.util.Arrays;
+
+class Q122买卖股票的最佳时机II{
+	public static void main(String[] args) {
+		Solution solution = new Q122买卖股票的最佳时机II().new Solution();
+		int[] prices = new int[]{7,1,5,3,6,4};
+		System.out.println(solution.maxProfit(prices));
+		
+	}
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxProfit(int[] prices) {
+		int n = prices.length;
+		if(n < 2){
+			return 0;
+		}
+		int[][] dp = new int[n][2];
+
+		dp[0][0] = 0;
+		dp[0][1] = -prices[0];
+		for(int i = 1; i < n; i++){
+			dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+			dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+		}
+		return dp[n - 1][0];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
