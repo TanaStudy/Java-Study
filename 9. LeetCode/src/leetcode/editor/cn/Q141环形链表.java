c0b5073c21615a081b06b3373a73eb4cae6af844
TanaStudy <tanzhihan3@gmail.com>
2022-01-31 12:54:33 +0800

    Q1141

---
+++ 9. LeetCode/src/leetcode/editor/cn/Q141环形链表.java
@@ -0,0 +1,96 @@
+package leetcode.editor.cn;
+
+//给你一个链表的头节点 head ，判断链表中是否有环。 
+//
+// 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到
+//链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。 
+//
+// 如果链表中存在环，则返回 true 。 否则，返回 false 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：head = [3,2,0,-4], pos = 1
+//输出：true
+//解释：链表中有一个环，其尾部连接到第二个节点。
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：head = [1,2], pos = 0
+//输出：true
+//解释：链表中有一个环，其尾部连接到第一个节点。
+// 
+//
+// 示例 3： 
+//
+// 
+//
+// 
+//输入：head = [1], pos = -1
+//输出：false
+//解释：链表中没有环。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目范围是 [0, 10⁴] 
+// -10⁵ <= Node.val <= 10⁵ 
+// pos 为 -1 或者链表中的一个 有效索引 。 
+// 
+//
+// 
+//
+// 进阶：你能用 O(1)（即，常量）内存解决此问题吗？ 
+// Related Topics 哈希表 链表 双指针 👍 1290 👎 0
+
+import java.awt.*;
+
+public class Q141环形链表{
+	public static void main(String[] args) {
+		Solution solution = new Q141环形链表().new Solution();
+		
+	}
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+// https://leetcode-cn.com/problems/linked-list-cycle/solution/huan-xing-lian-biao-by-leetcode-solution/
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+    	if(head == null || head.next == null){
+    		return false;
+		}
+    	ListNode slow = head;
+    	ListNode fast = head.next;
+    	while (slow != fast){
+    		if(fast == null || fast.next == null){
+    			return false;
+			}
+    		slow = slow.next;
+    		fast = fast.next.next;
+		}
+    	return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
