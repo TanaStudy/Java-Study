f59e195bdd59fae342c35599b311f52fb096e2ea
TanaStudy <tanzhihan3@gmail.com>
2022-02-21 10:54:41 +0800

    Q167

---
+++ 9. LeetCode/src/leetcode/editor/cn/Q167两数之和 II - 输入有序数组.java
@@ -0,0 +1,71 @@
+package leetcode.editor.cn;
+
+//给定一个已按照 非递减顺序排列 的整数数组 numbers ，请你从数组中找出两个数满足相加之和等于目标数 target 。 
+//
+// 函数应该以长度为 2 的整数数组的形式返回这两个数的下标值。numbers 的下标 从 1 开始计数 ，所以答案数组应当满足 1 <= answer[0]
+// < answer[1] <= numbers.length 。 
+//
+// 你可以假设每个输入 只对应唯一的答案 ，而且你 不可以 重复使用相同的元素。 
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：numbers = [2,7,11,15], target = 9
+//输出：[1,2]
+//解释：2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：numbers = [2,3,4], target = 6
+//输出：[1,3]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：numbers = [-1,0], target = -1
+//输出：[1,2]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 2 <= numbers.length <= 3 * 10⁴ 
+// -1000 <= numbers[i] <= 1000 
+// numbers 按 非递减顺序 排列 
+// -1000 <= target <= 1000 
+// 仅存在一个有效答案 
+// 
+// Related Topics 数组 双指针 二分查找 👍 641 👎 0
+
+class Q167两数之和II输入有序数组{
+	public static void main(String[] args) {
+		Solution solution = new Q167两数之和II输入有序数组().new Solution();
+		
+	}
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+    	int left = 0, right = numbers.length - 1;
+    	while (left < right){
+    		int sum = numbers[left] + numbers[right];
+    		if(sum == target){
+    			return new int[]{left + 1, right + 1};
+			}else if(sum < target){
+    			left++;
+			}else if(sum > target){
+    			right--;
+			}
+		}
+    	return null;
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
