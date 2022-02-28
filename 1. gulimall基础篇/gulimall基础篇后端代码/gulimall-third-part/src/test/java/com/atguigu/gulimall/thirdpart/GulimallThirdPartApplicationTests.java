0628e9dc38b27f93f4693da441621e621cd8d615
TanaStudy <tanzhihan3@gmail.com>
2022-02-28 15:40:01 +0800

    modify aliyun key

--- 1. gulimall基础篇/gulimall基础篇后端代码/gulimall-third-part/src/test/java/com/atguigu/gulimall/thirdpart/GulimallThirdPartApplicationTests.java
+++ 1. gulimall基础篇/gulimall基础篇后端代码/gulimall-third-part/src/test/java/com/atguigu/gulimall/thirdpart/GulimallThirdPartApplicationTests.java
@@ -23,8 +23,8 @@ class GulimallThirdPartApplicationTests {
 //		// yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
 //		String endpoint = "oss-cn-beijing.aliyuncs.com";
 //// 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-//		String accessKeyId = "LTAI5tGj5qKyesezwVkZUjWw";
-//		String accessKeySecret = "2I52vTgLJGnh6jNmBfoBQdPetR8RVg";
+//		String accessKeyId = "****";
+//		String accessKeySecret = "****";
 
 // 创建OSSClient实例。
 //		OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
