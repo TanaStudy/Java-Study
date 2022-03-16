56464176eb8586bf0f40320315bbca3b8fe1b648
TanaStudy <tanzhihan3@gmail.com>
2022-03-16 15:13:26 +0800

    OJ

---
+++ 8. Demo/Java/src/main/OJ/处理输入/test.java
@@ -0,0 +1,16 @@
+package 处理输入;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        String line1 = cin.nextLine();
+        String[] line2 = cin.nextLine().split(" ");
+        System.out.println(line1);
+        System.out.println(Arrays.toString(line2));
+
+        
+    }
+}
