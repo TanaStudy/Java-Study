62ddccf1c6b8d75bc13c9b9dda87df2111d277b9
TanaStudy <tanzhihan3@gmail.com>
2022-04-02 15:18:26 +0800

    Node

---
+++ 9. LeetCode/src/leetcode/editor/cn/Node.java
@@ -0,0 +1,19 @@
+package leetcode.editor.cn;
+
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val,Node _left,Node _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+}
