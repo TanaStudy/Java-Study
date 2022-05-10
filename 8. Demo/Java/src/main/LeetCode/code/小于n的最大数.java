bf7a3ec7d22d0810c52c8eda75eee99e1c71af78
TanaStudy <tanzhihan3@gmail.com>
2022-05-10 16:59:13 +0800

    小于n的最大数

--- 8. Demo/Java/src/main/LeetCode/code/小于n的最大数.java
+++ 8. Demo/Java/src/main/LeetCode/code/小于n的最大数.java
@@ -5,10 +5,10 @@ import java.util.Arrays;
 /*
 给定一个数n，如23121;给定—组数字A如{2，4，9}，求由A中元素组成的、小于n的最大数，如小于23121的最大数为22999,
  */
-public class Q1 {
+public class 小于n的最大数 {
 
     // 23121   [2,4,9] ---- > 22999
-    public static int findMax(int target, int[] nums) {
+    public static int findMax1(int target, int[] nums) {
         char[] str = String.valueOf(target).toCharArray();
         int len = str.length;
         Arrays.sort(nums);
@@ -78,11 +78,41 @@ public class Q1 {
         }
         return l;
     }
+    public static String minValue(int value, int[] nums) {
+        Arrays.sort(nums);
+        boolean flag = false;
+        String valueStr = String.valueOf(value);
+        StringBuilder result = new StringBuilder();
+        for (int i = 0;i < valueStr.length();i++) {
+            int current = valueStr.charAt(i) - '0';
+            for (int j = 0;j < nums.length;j++) {
+                if (flag) {
+                    // 如果已经确定当前结果小于 value ，那么剩下的几位全部使用最大值
+                    result.append(nums[nums.length - 1]);
+                    // 直接退出，因为不用与数组其他的元素进行对比了，可以比较下一位了
+                    break;
+                } else if (current == nums[j]) {
+                    // 如果发现当前结果的第 i 位刚好等于 nums[j] ，那么直接将 nums[j] 拼接到结果中
+                    result.append(nums[j]);
+                    break;
+                } else if (current < nums[j]) {
+                    // 如果发现当前位小于 nums[j] ，那么尝试拼接前一位，并将 flag 置为 true
+                    // 这里的 nums[j - 1] 一定小于 current
+                    if (j - 1 >= 0) {
+                        result.append(nums[j - 1]);
+                    }
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        return result.toString();
+    }
 
     public static void main(String[] args) {
         int n = 23121;
         int[] nums = new int[]{2, 4, 9};
-        int res = findMax(n, nums);
+        String res = minValue(n, nums);
         System.out.println(res);
     }
 }
