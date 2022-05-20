97f61baa8855b7c20d66af999018afaf5fc7c136
TanaStudy <tanzhihan3@gmail.com>
2022-05-20 10:11:24 +0800

    Q111二叉树的最小深度

--- 9. LeetCode/src/leetcode/editor/cn/Q111二叉树的最小深度.java
+++ 9. LeetCode/src/leetcode/editor/cn/Q111二叉树的最小深度.java
@@ -53,21 +53,30 @@ public class Q111二叉树的最小深度{
  *     }
  * }
  */
+// 递归
+// 叶子节点的定义是左孩子和右孩子都为 null 时叫做叶子节点
+// 当 root 节点左右孩子都为空时，返回 1
+// 当 root 节点左右孩子有一个为空时，返回不为空的孩子节点的深度
+// 当 root 节点左右孩子都不为空时，返回左右孩子较小深度的节点值
 // https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/solution/li-jie-zhe-dao-ti-de-jie-shu-tiao-jian-by-user7208/
 class Solution {
     public int minDepth(TreeNode root) {
 		if(root == null){
 			return 0;
 		}
+		// 这道题递归条件里分为三种情况
+		// 1.左孩子和有孩子都为空的情况，说明到达了叶子节点，直接返回1即可
 		if(root.left == null && root.right == null){
 			return 1;
 		}
+		// 2.如果左孩子和由孩子其中一个为空，那么需要返回比较大的那个孩子的深度
 		int left = minDepth(root.left);
 		int right = minDepth(root.right);
-
+        // 这里其中一个节点为空，说明m1和m2有一个必然为0，所以可以返回m1 + m2 + 1;
 		if(root.left == null || root.right == null){
 			return left + right + 1;
 		}
+		// 3.最后一种情况，也就是左右孩子都不为空，返回最小深度+1即可
 		return Math.min(left, right) + 1;
     }
 }
