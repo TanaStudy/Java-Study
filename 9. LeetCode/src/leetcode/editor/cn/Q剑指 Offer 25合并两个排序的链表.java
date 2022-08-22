42118086c2c978b7b69ad8d34f046e1a5cc7a896
TanaStudy <tanzhihan3@gmail.com>
2022-08-22 09:38:18 +0800

    Q剑指 Offer 25合并两个排序的链表

---
+++ 9. LeetCode/src/leetcode/editor/cn/Q剑指 Offer 25合并两个排序的链表.java
@@ -0,0 +1,54 @@
+package leetcode.editor.cn;
+
+//输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。 
+//
+// 示例1： 
+//
+// 输入：1->2->4, 1->3->4
+//输出：1->1->2->3->4->4 
+//
+// 限制： 
+//
+// 0 <= 链表长度 <= 1000 
+//
+// 注意：本题与主站 21 题相同：https://leetcode-cn.com/problems/merge-two-sorted-lists/ 
+// Related Topics 递归 链表 👍 268 👎 0
+
+import java.util.List;
+
+public class Q剑指Offer25合并两个排序的链表{
+	public static void main(String[] args) {
+		Solution solution = new Q剑指Offer25合并两个排序的链表().new Solution();
+		
+	}
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+// https://leetcode.cn/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof/solution/mian-shi-ti-25-he-bing-liang-ge-pai-xu-de-lian-b-2/
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		ListNode dum = new ListNode(0), cur = dum;
+		while (l1 != null && l2 != null) {
+			if (l1.val < l2.val) {
+				cur.next = l1;
+				l1 = l1.next;
+			}
+			else {
+				cur.next = l2;
+				l2 = l2.next;
+			}
+			cur = cur.next;
+		}
+		cur.next = l1 != null ? l1 : l2;
+		return dum.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ 9. LeetCode/src/leetcode/editor/cn/doc/content/Q剑指 Offer 25合并两个排序的链表.md
@@ -0,0 +1,13 @@
+<p>输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。</p>
+
+<p><strong>示例1：</strong></p>
+
+<pre><strong>输入：</strong>1-&gt;2-&gt;4, 1-&gt;3-&gt;4
+<strong>输出：</strong>1-&gt;1-&gt;2-&gt;3-&gt;4-&gt;4</pre>
+
+<p><strong>限制：</strong></p>
+
+<p><code>0 &lt;= 链表长度 &lt;= 1000</code></p>
+
+<p>注意：本题与主站 21 题相同：<a href="https://leetcode-cn.com/problems/merge-two-sorted-lists/">https://leetcode-cn.com/problems/merge-two-sorted-lists/</a></p>
+<div><div>Related Topics</div><div><li>递归</li><li>链表</li></div></div><br><div><li>👍 268</li><li>👎 0</li></div>
